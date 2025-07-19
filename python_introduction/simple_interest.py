4ff9409191f004ac7a6076d80ae1d7942046489b
Beatrice Labe Lary <beatricelabelary@gmail.com>
2025-07-19 16:14:44 +0200

    Calculating the interest earned on an investment

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#calculating the interest
+interest = principal * rate * time
+
+#printing the result
+print("The simple interest is:", interest)
