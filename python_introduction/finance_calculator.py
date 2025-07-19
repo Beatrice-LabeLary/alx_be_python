224c4841f65e3df017db18165908517ebd319ae6
Beatrice Labe Lary <beatricelabelary@gmail.com>
2025-07-19 16:56:00 +0200

    fixing calculations

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,11 @@
 #promtping user to get data
-monthlyIncome = input("Enter your monthly income: ")
-monthlyExpenses = input("Enter your total monthly expenses: ")
+monthly_income = input("Enter your monthly income: ")
+monthly_expenses = input("Enter your total monthly expenses: ")
 
 #Calculions
-monthlySavings = int(monthlyIncome) - int(monthlyExpenses)
-projectedSavings = int(monthlySavings * 12 + (monthlySavings * 12 * 0.05))
+monthly_savings = int(monthly_income) - int(monthly_expenses)
+projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
 #printing results
-print("Your monthly savings are $", monthlySavings, ".")
-print("Projected savings after one year, with interest, is: $", projectedSavings, ".")
+print("Your monthly savings are $", monthly_savings, ".")
+print("Projected savings after one year, with interest, is: $", projected_savings, ".")
